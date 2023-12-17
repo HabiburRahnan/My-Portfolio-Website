5fa3e24ed4040060137dd658588dbdf7635cb8f6
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2023-12-17 21:16:55 +0600

    scroll and animation code

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,19 @@
+import ScrollToTop from "react-scroll-to-top";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
+import Scroll from "../Components/Scroll/Scroll";
 
 const Main = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Scroll></Scroll>
       <Outlet></Outlet>
+      <ScrollToTop smooth />
+
       <Footer></Footer>
-    </div> 
+    </div>
   );
 };
 
