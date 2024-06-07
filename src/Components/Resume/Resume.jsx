637aa9fd4c06bab7ef4b9b7f4a9b71843f253828
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2024-06-07 15:50:35 +0600

    resume updated

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -4,18 +4,18 @@ const Resume = () => {
   return (
     <div className="md:flex justify-center items-center">
       <div>
-        <img src="https://i.ibb.co/MnJWZ8q/resume.png"></img>
+        <img src="https://i.ibb.co/NmRGg1D/skin.png"></img>
       </div>
       <div className=" grid justify-center items-center gap-10  ml-10">
         <Link
           className="border border-pink-600 p-10 text-xl rounded"
-          to="https://drive.google.com/file/d/1c5MeYbPBQ639Z_WZOvKqll4z5tF0PUsW/view?usp=sharing"
+          to="https://drive.google.com/file/d/1Q1NRKAaUSuw7hsQPpShHaM85yxH9sJ4E/view?usp=sharing"
         >
           Views Resume Fornt-End
         </Link>
         <Link
           className="border border-pink-600 p-10 text-xl rounded"
-          to="https://drive.google.com/file/d/1_VM9HF1cIatcmYXM0UnxX5xLknA5o9k9/view?usp=sharing"
+          to="https://drive.google.com/file/d/1bGq8AkQquOMOPTOTm3d39I4T1NQxfJYT/view?usp=sharing"
         >
           Views Resume MERN Stack
         </Link>
